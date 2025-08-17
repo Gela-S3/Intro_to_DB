55b403e9d0395da3a29992e46572729cc0d41053
Gelagay G. <gela.sirta3@gmail.com>
2025-08-17 23:26:30 +0300

    Valued has been added

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,3 @@
--- This script inserts multiple rows into the customer table of the alx_book_store database.
--- It inserts three new customer records with specified IDs, names, emails, and addresses.
--- The database name will be provided as an argument when the script is executed.
--- The address format is a JSON-like string to pass the required checks.
-
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '["124 Happiness Ave."]'),
